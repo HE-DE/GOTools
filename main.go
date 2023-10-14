f99ccd16b9d0ae6c1c9593e3649a856f59a61cc7
HE-DE <3029511904@qq.com>
2023-10-14 19:53:59 +0800

    fix: 【-】删除部分无意义的输出

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"goscanner/logger"
+	"goscanner/scan"
+	"goscanner/utils"
+	"strconv"
+)
+
+func main() {
+	args := utils.ParseArgs()
+	l := logger.NewLogger()
+
+	if args[0] == "sc" {
+		beginport, _ := strconv.Atoi(args[1])
+		endport, _ := strconv.Atoi(args[2])
+		threads, _ := strconv.Atoi(args[3])
+		l.Info("正在使用端口扫描器扫描端口...")
+		ts := scan.ScannerInit(beginport, endport, threads)
+		ts.ScanAll()
+		result := ts.GetOpenPorts()
+		fmt.Println()
+		l.Info("扫描完成！")
+		for _, port := range result {
+			l.Notice(fmt.Sprintf("%d 端口开放！", port))
+		}
+	} else if args[0] == "ps" {
+		l.Info("正在使用Ping扫描器扫描主机...")
+		success, _ := strconv.Atoi(args[2])
+		ps := scan.PingScannerInit(args[1], success)
+		ps.ScanAll()
+		result := ps.GetAliveIp()
+		fmt.Println()
+		l.Info("扫描完成！")
+		for _, ip := range result {
+			l.Notice(fmt.Sprintf("%s 主机存活!", ip))
+		}
+	} else if args[0] == "ms" {
+		l.Info("正在使用Mysql扫描器扫描主机...")
+		ms := scan.MysqlscannerInit(args[1], args[2], args[3])
+		ms.ScanAll()
+		result := ms.GetMetadata()
+		fmt.Println()
+		l.Info("扫描完成！")
+		for _, meta := range result {
+			l.Notice(meta)
+		}
+	} else if args[0] == "fe" {
+		l.Info("正在使用AES加密文件...")
+		if len(args[3]) != 16 && len(args[3]) != 24 && len(args[3]) != 32 {
+			l.Error("密钥长度错误，请检查！")
+			return
+		}
+		keys := []byte(args[3])
+		fe := scan.FileEncoderInit(args[1], keys, args[2])
+		err := fe.Encrypt()
+		if err != nil {
+			l.Error(err.Error())
+			return
+		}
+		l.Info("加密完成！")
+	} else if args[0] == "fd" {
+		l.Info("正在使用AES解密文件...")
+		if len(args[3]) != 16 && len(args[3]) != 24 && len(args[3]) != 32 {
+			l.Error("密钥长度错误，请检查！")
+			return
+		}
+		keys := []byte(args[3])
+		fd := scan.FileDecoderInit(args[1], keys, args[2])
+		err := fd.Decrypt()
+		if err != nil {
+			l.Error(err.Error())
+			return
+		}
+		l.Info("解密完成！")
+	}
+}
--- scan/fileEncoder.go
+++ scan/fileEncoder.go
@@ -2,7 +2,6 @@ package scan
 
 import (
 	"bufio"
-	"fmt"
 	"goscanner/utils"
 	"os"
 )
@@ -29,7 +28,6 @@ func (fe *FileEncoder) Encrypt() error {
 	defer f.Close()
 	fInfo, _ := f.Stat()
 	fLen := fInfo.Size()
-	fmt.Println("待处理文件大小:", fLen)
 	maxLen := 1024 * 100 //100Kb  每 100Kb 加密一次
 	var forNum int64 = 0
 	getLen := fLen
@@ -37,13 +35,11 @@ func (fe *FileEncoder) Encrypt() error {
 	if fLen > int64(maxLen) {
 		getLen = int64(maxLen)
 		forNum = fLen / int64(maxLen)
-		fmt.Println("需要加密次数：", forNum+1)
 	}
 
 	// 加密后的存储文件
 	ff, err := os.OpenFile(fe.FOutPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println("写入失败")
 		return err
 	}
 	defer ff.Close()
@@ -53,12 +49,10 @@ func (fe *FileEncoder) Encrypt() error {
 		a := make([]byte, getLen)
 		n, err := f.Read(a)
 		if err != nil {
-			fmt.Println("文件读取失败")
 			return err
 		}
 		getByte, err := utils.EncryptByAes(a[:n], fe.Key)
 		if err != nil {
-			fmt.Println("加密错误")
 			return err
 		}
 		//换行处理
@@ -68,7 +62,5 @@ func (fe *FileEncoder) Encrypt() error {
 		buf.WriteString(string(getBytes[:]))
 		buf.Flush()
 	}
-	ffInfo, _ := ff.Stat()
-	fmt.Printf("文件加密成功，生成文件名为：%s 文件大小为：%v Byte \n", ffInfo.Name(), ffInfo.Size())
 	return nil
 }
